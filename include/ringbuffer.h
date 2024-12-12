45947f395a504090d2e0f34c0575c9652d38e871
Stadnik Andrey <halfbox@gmail.com>
2024-12-12 16:23:54 +0300

    L1 data cache line size alignment. Short performance improvement for Release build

--- include/ringbuffer.h
+++ include/ringbuffer.h
@@ -3,6 +3,7 @@
 #include <cstddef>
 #include <vector>
 #include <stdexcept>
+#include <new>
 
 namespace ringbuffer {
 
@@ -11,12 +12,17 @@ class ring_buffer {
 public:
     explicit ring_buffer(size_t _capacity) : storage_(_capacity), capacity_(_capacity) {
         assert(_capacity != 0);
-        if (_capacity == 0) throw std::runtime_error("ring_buffer capacity must be greate than zero");
+        if (_capacity == 0)  {
+            throw std::runtime_error("ring_buffer capacity must be greate than zero");
+        }
     }
+
+    ring_buffer(const ring_buffer &) = delete;
+    ring_buffer &operator=(const ring_buffer &) = delete;
 
     template <typename ... Args>
     bool push(Args && ... args) noexcept {
-        if (next_tail_ == head_.load(std::memory_order_acquire)) {
+        if (next_tail_ == head_.load(std::memory_order_relaxed)) {
             return false;
         }
 
@@ -27,28 +33,30 @@ public:
         return true;
     }
 
-  bool pop(T& _value) noexcept {
-    auto cached = head_.load(std::memory_order_relaxed);
-    if (tail_.load(std::memory_order_acquire) == cached) { return false; }
+    bool pop(T& _value) noexcept {
+        auto cached_head = head_.load(std::memory_order_relaxed);
 
-    _value = std::move(storage_[cached]);
-    head_.store(get_next(cached), std::memory_order_release);
+        if (tail_.load(std::memory_order_acquire) == cached_head) { 
+            return false; 
+        }
 
-    return true;
-  }
+        _value = std::move(storage_[cached_head]);
+        head_.store(get_next(cached_head), std::memory_order_relaxed);
+        return true;
+    }
 
 private:
 
-  inline size_t get_next(size_t _slot) const noexcept {
-    ++_slot;
-    return (_slot == capacity_) ? 0 : _slot;
-  }
+    inline size_t get_next(size_t _slot) const noexcept {
+        ++_slot;
+        return (_slot == capacity_) ? 0 : _slot;
+    }
 
     std::vector<T> storage_;
     size_t capacity_ = { 0 };
-    std::atomic<size_t> head_ = { 0 };
-    std::atomic<size_t> tail_ = { 0 };
-    size_t next_tail_ = tail_ + 1;
+    alignas(std::hardware_destructive_interference_size) std::atomic<size_t> head_ = { 0 };
+    alignas(std::hardware_destructive_interference_size) std::atomic<size_t> tail_ = { 0 };
+    size_t next_tail_ = { tail_ + 1 };
 };
 
 } // ringbuffer
